1a419c41bdb499a20641d8ef6eb4d525b24aa024
Kaida Masaki <kaida@adadevelopersacademy.org>
2023-01-02 16:20:12 -0800

    Remove mention of the umbrella from config/config.exs

--- config/config.exs
+++ config/config.exs
@@ -1,12 +1,10 @@
-# This file is responsible for configuring your umbrella
-# and **all applications** and their dependencies with the
-# help of the Config module.
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Config module.
 #
-# Note that all applications in your umbrella share the
-# same configuration and dependencies, which is why they
-# all use the same configuration file. If you want different
-# configurations or dependencies per app, it is best to
-# move said applications out of the umbrella.
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
 import Config
 
 # Configure tzdata
