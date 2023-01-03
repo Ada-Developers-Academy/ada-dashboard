d3b0a6f944d6187d5a459de902bf631c89b44d4b
Kaida Masaki <kaida@adadevelopersacademy.org>
2023-01-03 15:45:37 -0800

    Fix config ordering issue

--- config/config.exs
+++ config/config.exs
@@ -53,9 +53,9 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# Calendar Config
+config :dashboard, DashboardWeb.CalendarUpdater, interval_seconds: 300
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
-
-# Calendar Config
-config :dashboard, DashboardWeb.CalendarUpdater, interval_seconds: 300
--- config/dev.exs
+++ config/dev.exs
@@ -73,3 +73,6 @@ config :phoenix, :plug_init_mode, :runtime
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
 config :phoenix, :stacktrace_depth, 20
+
+# Calendar Config
+config :dashboard, DashboardWeb.CalendarUpdater, interval_seconds: 20
